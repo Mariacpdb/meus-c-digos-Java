0cbf1b74d2ffcb30212b45fdbcd249d95d7f9f69
Mariacpdb <161085828+Mariacpdb@users.noreply.github.com>
2024-03-20 15:06:09 -0300

    Add files via upload

---
+++ Maior nmr.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class Maior nmr {
+  
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int numb1 = scanner.nextInt();
+    int numb2 = scanner.nextInt();
+    int maiornumb;
+    if (numb1 > numb2) {
+      maiornumb = numb1;
+    } else {
+      maiornumb = numb2;
+    }
+    System.out.println("\n" + numb1);
+    System.out.println("\n" + numb2);
+    System.out.println("====================");
+    System.out.println("\n O maior numero e: " + 
+    maiornumb);
+      
+    scanner.close();
+    
+    
+    
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
